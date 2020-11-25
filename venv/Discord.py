67db7295ce8b2c27ca6e80cc790b8cf2d7602395
Ko-Gyeongtae <kokt0203@naver.com>
2020-11-25 09:54:56 +0900

    add today meal

--- venv/Discord.py
+++ venv/Discord.py
@@ -27,8 +27,12 @@ class MealBot(discord.Client):
             return None
 
         if message.content == "!오늘급식":
-            channer = message.channel
-            msg = date + "아침" + breakfast + "점심" + lunch + "저녁" + dinner
+            channel = message.channel
+            msg = "```\n" + "####" + date + "####"
+            msg += "\n\n*** 아침 ***\n" + ' ' + breakfast
+            msg += "\n\n*** 점심 ***\n" + ' ' + lunch
+            msg += "\n\n*** 저녁 ***\n" + ' ' + dinner
+            msg += "\n\n```"
             await channel.send(msg)
             return None
 
@@ -42,16 +46,21 @@ if __name__ == "__main__":
 
     # soup = BeautifulSoup(html, 'html.parser')
     meal = literal_eval(html)
-    breakfast_dic = meal[date]['breakfast']
-    lunch_dic = meal[date]['lunch']
-    dinner_dic = meal[date]['dinner']
-
-    breakfast = str(breakfast_dic)
-    breakfast = breakfast.translate({ord('['): '', ord(']'): '', ord("'"): '', ord(','): '\n'})
-    lunch = str(lunch_dic)
-    lunch = lunch.translate({ord('['): '', ord(']'): '', ord("'"): '', ord(','): '\n'})
-    dinner = str(dinner_dic)
-    dinner = dinner.translate({ord('['): '', ord(']'): '', ord("'"): '', ord(','): '\n'})
-
+    if "breakfast" in str(meal):
+        breakfast_dic = meal[date]['breakfast']
+        breakfast = str(breakfast_dic)
+        breakfast = breakfast.translate({ord('['): '', ord(']'): '', ord("'"): '', ord(','): '\n'})
+    if "lunch" in str(meal):
+        lunch_dic = meal[date]['lunch']
+        lunch = str(lunch_dic)
+        lunch = lunch.translate({ord('['): '', ord(']'): '', ord("'"): '', ord(','): '\n'})
+    if "dinner" in str(meal):
+        dinner_dic = meal[date]['dinner']
+        dinner = str(dinner_dic)
+        dinner = dinner.translate({ord('['): '', ord(']'): '', ord("'"): '', ord(','): '\n'})
+    print(meal)
+    print(breakfast)
+    print(lunch)
+    print(dinner)
     client = MealBot()
     client.run(token)
