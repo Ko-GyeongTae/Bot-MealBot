df335cb6ded25a439f7ed1c761a8dd02b988c122
Ko-Gyeongtae <kokt0203@naver.com>
2020-11-29 21:23:38 +0900

    take live menu version 1.1.0

--- venv/Discord.py
+++ venv/Discord.py
@@ -11,6 +11,8 @@ breakfast = "x"
 lunch = "x"
 dinner = "x"
 
+now = "X"
+tommorrow = "x"
 
 class MealBot(discord.Client):
     channel = "NULL"
@@ -27,12 +29,55 @@ class MealBot(discord.Client):
         # sender == bot ? None
         if message.author.bot:
             return None
+        now = datetime.datetime.now()
+        tommorrow = now + datetime.timedelta(days=1)
+
+        date = now.strftime('%Y-%m-%d')
+        t_date = tommorrow.strftime('%Y-%m-%d')
+
+        # Debug Date
+        #date = '2020-08-19'
+        #t_date = '2020-08-20'
+
         log = str(message.author) + ' :: ' + message.content
         log += ' :: ' + str(time.localtime().tm_year) + '-' + str(time.localtime().tm_mon) + '-' \
             + str(time.localtime().tm_mday)
         log += ' :: ' + str(time.localtime().tm_hour) + ':' + str(time.localtime().tm_min) + ':' \
                + str(time.localtime().tm_sec)
+        url = f'https://api.dsm-dms.com/meal/{date}'
+        t_url = f'https://api.dsm-dms.com/meal/{t_date}'
+        html = requests.get(url).text
+        t_html = requests.get(t_url).text
+
+        meal = literal_eval(html)
+        if "breakfast" in str(meal):
+            breakfast_dic = meal[date]['breakfast']
+            breakfast = str(breakfast_dic)
+            breakfast = breakfast.translate({ord('['): '', ord(']'): '', ord("'"): '', ord(','): '\n'})
+        if "lunch" in str(meal):
+            lunch_dic = meal[date]['lunch']
+            lunch = str(lunch_dic)
+            lunch = lunch.translate({ord('['): '', ord(']'): '', ord("'"): '', ord(','): '\n'})
+        if "dinner" in str(meal):
+            dinner_dic = meal[date]['dinner']
+            dinner = str(dinner_dic)
+            dinner = dinner.translate({ord('['): '', ord(']'): '', ord("'"): '', ord(','): '\n'})
+
+        t_meal = literal_eval(t_html)
+        if "breakfast" in str(t_meal):
+            t_breakfast_dic = t_meal[t_date]['breakfast']
+            t_breakfast = str(breakfast_dic)
+            t_breakfast = breakfast.translate({ord('['): '', ord(']'): '', ord("'"): '', ord(','): '\n'})
+        if "lunch" in str(t_meal):
+            t_lunch_dic = t_meal[t_date]['lunch']
+            t_lunch = str(lunch_dic)
+            t_lunch = lunch.translate({ord('['): '', ord(']'): '', ord("'"): '', ord(','): '\n'})
+        if "dinner" in str(t_meal):
+            t_dinner_dic = t_meal[t_date]['dinner']
+            t_dinner = str(dinner_dic)
+            t_dinner = dinner.translate({ord('['): '', ord(']'): '', ord("'"): '', ord(','): '\n'})
         print(log)
+
         if message.content == '!도움':
             channel = message.channel
             msg = "```\n!오늘급식 - 하루의 모든 급식 보여줌\n"
@@ -126,9 +171,9 @@ if __name__ == "__main__":
     date = now.strftime('%Y-%m-%d')
     t_date = tommorrow.strftime('%Y-%m-%d')
 
-    #Debug Date
-    date = '2020-08-19'
-    t_date = '2020-08-20'
+    # Debug Date
+    #date = '2020-08-19'
+    #t_date = '2020-08-20'
 
     url = f'https://api.dsm-dms.com/meal/{date}'
     t_url = f'https://api.dsm-dms.com/meal/{t_date}'
