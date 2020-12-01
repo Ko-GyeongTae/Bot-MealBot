749d24a46249ac5870f801ce038b42823ea72871
Ko-Gyeongtae <kokt0203@naver.com>
2020-12-01 21:13:00 +0900

    clean code

--- venv/Discord.py
+++ venv/Discord.py
@@ -5,12 +5,9 @@ import schedule
 import time
 from ast import literal_eval
 
-token = "token"
+token = "NzgwOTUxMzUwNDA1NDk2ODYz.X72jnw.6oGXqbb-xe1_B2hDaZycoIzqnJg"
 
-breakfast = "x"
-lunch = "x"
-dinner = "x"
-
+msg = "x"
 now = "X"
 tommorrow = "x"
 
@@ -66,16 +63,16 @@ class MealBot(discord.Client):
         t_meal = literal_eval(t_html)
         if "breakfast" in str(t_meal):
             t_breakfast_dic = t_meal[t_date]['breakfast']
-            t_breakfast = str(breakfast_dic)
-            t_breakfast = breakfast.translate({ord('['): '', ord(']'): '', ord("'"): '', ord(','): '\n'})
+            t_breakfast = str(t_breakfast_dic)
+            t_breakfast = t_breakfast.translate({ord('['): '', ord(']'): '', ord("'"): '', ord(','): '\n'})
         if "lunch" in str(t_meal):
             t_lunch_dic = t_meal[t_date]['lunch']
-            t_lunch = str(lunch_dic)
-            t_lunch = lunch.translate({ord('['): '', ord(']'): '', ord("'"): '', ord(','): '\n'})
+            t_lunch = str(t_lunch_dic)
+            t_lunch = t_lunch.translate({ord('['): '', ord(']'): '', ord("'"): '', ord(','): '\n'})
         if "dinner" in str(t_meal):
             t_dinner_dic = t_meal[t_date]['dinner']
-            t_dinner = str(dinner_dic)
-            t_dinner = dinner.translate({ord('['): '', ord(']'): '', ord("'"): '', ord(','): '\n'})
+            t_dinner = str(t_dinner_dic)
+            t_dinner = t_dinner.translate({ord('['): '', ord(']'): '', ord("'"): '', ord(','): '\n'})
         print(log)
 
         if message.content == '!도움':
@@ -125,9 +122,9 @@ class MealBot(discord.Client):
         if message.content == "!내일급식":
             channel = message.channel
             msg = "```\n" + "####" + t_date + "####"
-            msg += "\n\n*** 아침 ***\n" + ' ' + breakfast
-            msg += "\n\n*** 점심 ***\n" + ' ' + lunch
-            msg += "\n\n*** 저녁 ***\n" + ' ' + dinner
+            msg += "\n\n*** 아침 ***\n" + ' ' + t_breakfast
+            msg += "\n\n*** 점심 ***\n" + ' ' + t_lunch
+            msg += "\n\n*** 저녁 ***\n" + ' ' + t_dinner
             msg += "\n\n```"
             await channel.send(msg)
             return None
@@ -180,48 +177,12 @@ if __name__ == "__main__":
     html = requests.get(url).text
     t_html = requests.get(t_url).text
 
-    meal = literal_eval(html)
-    if "breakfast" in str(meal):
-        breakfast_dic = meal[date]['breakfast']
-        breakfast = str(breakfast_dic)
-        breakfast = breakfast.translate({ord('['): '', ord(']'): '', ord("'"): '', ord(','): '\n'})
-    if "lunch" in str(meal):
-        lunch_dic = meal[date]['lunch']
-        lunch = str(lunch_dic)
-        lunch = lunch.translate({ord('['): '', ord(']'): '', ord("'"): '', ord(','): '\n'})
-    if "dinner" in str(meal):
-        dinner_dic = meal[date]['dinner']
-        dinner = str(dinner_dic)
-        dinner = dinner.translate({ord('['): '', ord(']'): '', ord("'"): '', ord(','): '\n'})
-
-    t_meal = literal_eval(t_html)
-    if "breakfast" in str(t_meal):
-        t_breakfast_dic = t_meal[t_date]['breakfast']
-        t_breakfast = str(breakfast_dic)
-        t_breakfast = breakfast.translate({ord('['): '', ord(']'): '', ord("'"): '', ord(','): '\n'})
-    if "lunch" in str(t_meal):
-        t_lunch_dic = t_meal[t_date]['lunch']
-        t_lunch = str(lunch_dic)
-        t_lunch = lunch.translate({ord('['): '', ord(']'): '', ord("'"): '', ord(','): '\n'})
-    if "dinner" in str(t_meal):
-        t_dinner_dic = t_meal[t_date]['dinner']
-        t_dinner = str(dinner_dic)
-        t_dinner = dinner.translate({ord('['): '', ord(']'): '', ord("'"): '', ord(','): '\n'})
-
     #Debug code
 
     print(now)
     print(tommorrow)
     print(date)
     print(t_date)
-    print(meal)
-    print(t_meal)
-    print(breakfast)
-    print(t_breakfast)
-    print(lunch)
-    print(t_lunch)
-    print(dinner)
-    print(t_dinner)
 
     client = MealBot()
     client.run(token)
